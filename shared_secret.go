62da647dae31a3330fb9206e212847cff42c044d
sfomuseumbot <sfomuseumbot@localhost>
2024-04-01 13:56:50 -0700

    make shared secret handler work

--- shared_secret.go
+++ shared_secret.go
@@ -10,26 +10,31 @@ import (
 	"regexp"
 )
 
+// re_auth is a regular expression used to match HTTP `Authorization` headers.
 var re_auth *regexp.Regexp
 
 func init() {
 	ctx := context.Background()
-	RegisterAuthenticator(ctx, "none", NewSharedSecretAuthenticator)
-
-	re_auth = regexp.MustCompile(`Authorization:\s+Shared\s+(.*)`)
+	RegisterAuthenticator(ctx, "sharedsecret", NewSharedSecretAuthenticator)
+	re_auth = regexp.MustCompile(`^X-Shared\s+(\w+)`)
 }
 
-// type SharedSecretAuthenticator implements the Authenticator interface that always returns a "not authorized" error.
+// type SharedSecretAuthenticator implements the Authenticator interface to require a simple shared secret be passed
+// with all requests. This is not a sophisticated handler. There are no nonces or hashing of requests or anything like
+// that. It is a bare-bones supplementary authentication handler for environments that already implement their own
+// measures of access control.
 type SharedSecretAuthenticator struct {
 	Authenticator
 	secret string
 	logger *log.Logger
 }
 
-// NewSharedSecretAuthenticator implements the Authenticator interface that always returns a "not authorized" error.
-// configured by 'uri' which is expected to take the form of:
+// NewSharedSecretAuthenticator implements the Authenticator interface to ensure that requests contain a `X-Shared` HTTP `Authorization`
+// header configured by 'uri' which is expected to take the form of:
 //
 //	sharedsecret://{SECRET}
+//
+// Where {SECRET} is expected to be the shared secret passed by HTTP requests.
 func NewSharedSecretAuthenticator(ctx context.Context, uri string) (Authenticator, error) {
 
 	u, err := url.Parse(uri)
@@ -39,6 +44,10 @@ func NewSharedSecretAuthenticator(ctx context.Context, uri string) (Authenticato
 	}
 
 	secret := u.Host
+
+	if secret == "" {
+		return nil, fmt.Errorf("Missing or invalid secret")
+	}
 
 	logger := log.New(io.Discard, "", 0)
 
@@ -58,6 +67,7 @@ func (a *SharedSecretAuthenticator) WrapHandler(next http.Handler) http.Handler 
 		_, err := a.GetAccountForRequest(req)
 
 		if err != nil {
+			// log.Println(err)
 			http.Error(rsp, "Forbidden", http.StatusForbidden)
 			return
 		}
@@ -69,7 +79,7 @@ func (a *SharedSecretAuthenticator) WrapHandler(next http.Handler) http.Handler 
 	return http.HandlerFunc(fn)
 }
 
-// GetAccountForRequest returns an stub `Account` instance.
+// GetAccountForRequest returns an stub `Account` instance for requests that contain a valid `Authorization: X-Shared` header.
 func (a *SharedSecretAuthenticator) GetAccountForRequest(req *http.Request) (*Account, error) {
 
 	auth := req.Header.Get("Authorization")
@@ -108,7 +118,7 @@ func (a *SharedSecretAuthenticator) SignupHandler() http.Handler {
 	return notImplementedHandler()
 }
 
-// SetLogger is a no-op and does nothing.
+// SetLogger assign 'logger' to `a`.
 func (a *SharedSecretAuthenticator) SetLogger(logger *log.Logger) {
 	a.logger = logger
 }
